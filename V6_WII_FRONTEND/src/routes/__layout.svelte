9a356f1adaaa6411c808f8521b9644304a683cb0
Noka Tang <nokajaejin@gmail.com>
2023-05-10 22:12:14 +0800

    v6 draft

--- V6_WII_FRONTEND/src/routes/__layout.svelte
+++ V6_WII_FRONTEND/src/routes/__layout.svelte
@@ -112,15 +112,7 @@
 </svg>
         </svelte:fragment>
 </SidebarItem>
-					<SidebarItem
-						label="Quick View" href='/quickview'
-					active={activeUrl === '/quickview'}>
-					<svelte:fragment slot="icon">
-<svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="text-blue-500 mr-2 dark:text-pink-500 w-6 h-6">
-  <path d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" stroke-linecap="round" stroke-linejoin="round"></path>
-</svg>
-        </svelte:fragment>
-					</SidebarItem>
+
 					<SidebarDropdownWrapper
 						label="Networking"
 					>
@@ -155,40 +147,9 @@
 </svg>
         </svelte:fragment>
 						<SidebarDropdownItem label="OpenVPN" href='/openvpn' active={activeUrl === '/openvpn'}/>
-						<SidebarDropdownItem label="IPSec" />
+
 					</SidebarDropdownWrapper>
-					<SidebarDropdownWrapper
-						label="Cloud"
-					>
-					<svelte:fragment slot="icon">
-<svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 mr-2 dark:text-pink-500 w-6 h-6">
-  <path d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z" stroke-linecap="round" stroke-linejoin="round"></path>
-</svg>
-        </svelte:fragment>
-
-						<SidebarDropdownItem label="Azure" />
-						<SidebarDropdownItem label="AWS" />
-						<SidebarDropdownItem label="Generic" />
-					</SidebarDropdownWrapper>
-					<SidebarDropdownWrapper
-						label="Field Managment"
-					>
-
-					<svelte:fragment slot="icon">
-<svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="text-blue-500 mr-2 dark:text-pink-500 w-6 h-6">
-<path d="M21.75 6.75a4.5 4.5 0 01-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 11-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 016.336-4.486l-3.276 3.276a3.004 3.004 0 002.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852z" stroke-linecap="round" stroke-linejoin="round"></path>
-  <path d="M4.867 19.125h.008v.008h-.008v-.008z" stroke-linecap="round" stroke-linejoin="round"></path>
-
-</svg>
-        </svelte:fragment>
-
-
-
-
-						<SidebarDropdownItem label="Port Connection" />
-						<SidebarDropdownItem label="Protocol" />
-						<SidebarDropdownItem label="Modbus Transparent" />
-					</SidebarDropdownWrapper>
+		
 
 
 					<SidebarDropdownWrapper
@@ -202,15 +163,9 @@
 </svg>
         </svelte:fragment>
 
-						<SidebarDropdownItem label="Data Tag Pro" />
-						<SidebarDropdownItem label="Data Logger" />
-						<SidebarDropdownItem label="Event Engine" />
-						<SidebarDropdownItem label="Remote Service" />
 						<SidebarDropdownItem label="DREAMS" href='/DREAMS' active={activeUrl === '/DREAMS'}/>
-						<SidebarDropdownItem label="Docker Engine" />
 						<SidebarDropdownItem label="GPS Standalone" />
-						<SidebarDropdownItem label="Power Saving" />
-						<SidebarDropdownItem label="Protocol Converter" />
+
 					</SidebarDropdownWrapper>
 
 					<SidebarDropdownWrapper
